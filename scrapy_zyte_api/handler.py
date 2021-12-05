9c512016c7a52d569f932a2b0ef4476414b3271a
sortafreel <woutut@gmail.com>
2021-12-05 20:29:01 +0200

    Add input from request.meta.

--- scrapy_zyte_api/handler.py
+++ scrapy_zyte_api/handler.py
@@ -1,12 +1,11 @@
-import os
-
 from scrapy import Spider
 from scrapy.core.downloader.handlers.http import HTTPDownloadHandler
 from scrapy.crawler import Crawler
-from scrapy.http import Request, Response
+from scrapy.http import Request
 from scrapy.responsetypes import responsetypes
 from scrapy.settings import Settings
 from scrapy.utils.defer import deferred_from_coro
+from scrapy.utils.reactor import verify_installed_reactor
 from twisted.internet.defer import Deferred, inlineCallbacks
 from zyte_api.aio.client import AsyncClient
 
@@ -17,25 +16,47 @@ class ScrapyZyteAPIDownloadHandler(HTTPDownloadHandler):
         self._client: AsyncClient = AsyncClient()
         # TODO Think about concurrent requests implementation
         # TODO Think about sessions reusing
+        verify_installed_reactor(
+            "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+        )
+        # TODO Add custom stats to increase/monitor
+        self.stats = crawler.stats
 
     def download_request(self, request: Request, spider: Spider) -> Deferred:
-        return deferred_from_coro(self._download_request(request, spider))
+        if request.meta.get("zyte_api"):
+            return deferred_from_coro(self._download_request(request, spider))
+        else:
+            return super().download_request(request, spider)
 
     async def _download_request(self, request: Request, spider: Spider):
-        api_response = await self._client.request_raw({
-            'url': request.url,
-            'browserHtml': True
-        })
+        # TODO Add input validation through openapi schema or similar
+        # https://github.com/zytedata/zde-api-server/blob/master/server/src/main/resources/openapi/openapi.yaml
+        api_data = {"url": request.url, "browserHtml": True}
+        api_params = request.meta["zyte_api"]
+        if api_params.get("javascript"):
+            api_data["javascript"] = api_params["javascript"]
+        if api_params.get("geolocation"):
+            api_data["geolocation"] = api_params["geolocation"]
+        echo_data = api_params.get("echoData")
+        # TODO Decide how to validate echoData
+        if echo_data:
+            api_data["echoData"] = echo_data
+        # TODO Check where to pick jobId
+        api_response = await self._client.request_raw(api_data)
         body = api_response["browserHtml"].encode("utf-8")
-        # TODO Get headers somewhere? Or return just HTML (base Response) every time?
-        response_class = responsetypes.from_args(headers=request.headers, url=request.url, body=body)
+        # TODO Could the response be different from default Response (just HTML)?
+        response_class = responsetypes.from_args(
+            headers=request.headers, url=request.url, body=body
+        )
+        if echo_data:
+            response_class.meta = api_response["echoData"]
         return response_class(
             url=request.url,
-            # TODO Get headers and status somewhere?
+            # TODO Get headers and status somewhere or stick with default ones?
             status=200,
             headers={},
             body=body,
-            request=request
+            request=request,
         )
 
     @inlineCallbacks
