7575b6e364db7fbccfae71d29e6d9fa05dde80ea
sortafreel <woutut@gmail.com>
2021-12-20 00:24:51 +0200

    Refactor zyte_api parameters processing.

--- scrapy_zyte_api/handler.py
+++ scrapy_zyte_api/handler.py
@@ -17,7 +17,9 @@ logger = logging.getLogger("scrapy-zyte-api")
 
 
 class ScrapyZyteAPIDownloadHandler(HTTPDownloadHandler):
-    def __init__(self, settings: Settings, crawler: Crawler, client: AsyncClient):
+    def __init__(
+        self, settings: Settings, crawler: Crawler, client: AsyncClient = None
+    ):
         super().__init__(settings=settings, crawler=crawler)
         self._client: AsyncClient = client if client else AsyncClient()
         # verify_installed_reactor(
@@ -36,29 +38,42 @@ class ScrapyZyteAPIDownloadHandler(HTTPDownloadHandler):
 
     async def _download_request(self, request: Request, spider: Spider):
         api_data = {"url": request.url, "browserHtml": True}
+        allowed_keys = {"javascript", "geolocation", "echoData"}
         api_params = request.meta["zyte_api"]
-        if api_params.get("javascript"):
-            api_data["javascript"] = api_params["javascript"]
-        if api_params.get("geolocation"):
-            api_data["geolocation"] = api_params["geolocation"]
-        echo_data = api_params.get("echoData")
-        # TODO Decide how to validate echoData
-        if echo_data:
-            api_data["echoData"] = echo_data
+        if not isinstance(api_params, dict):
+            raise TypeError(
+                "zyte_api parameters in the request meta should be "
+                f"provided as dictionary (got {type(api_params)} instead)"
+            )
+        for key, value in api_params.items():
+            if key not in allowed_keys:
+                logger.warning(
+                    f"Key `{key}` isn't allowed in zyte_api parameters, skipping."
+                )
+                continue
+            # Protect default settings (request url and browserHtml)
+            if key in api_data:
+                logger.warning(
+                    "Key `{key}` is already in zyte_api parameters "
+                    f"({api_data[key]}) and can't be overwritten, skipping."
+                )
+                continue
+            # TODO Decide how to validate echoData or do I need to validate it at all?
+            api_data[key] = value
         # TODO Check where to pick jobId
         # TODO Handle request errors
         api_response = await self._client.request_raw(api_data, session=self._session)
         self._stats.inc_value("scrapy-zyte-api/request_count")
         body = api_response["browserHtml"].encode("utf-8")
-
-        # TODO Add retrying support
+        # TODO Add retrying support?
         return Response(
             url=request.url,
-            status=api_response["statusCode"],
+            # TODO Add status code data to the API
+            status=200,
             body=body,
             request=request,
-            # API provides no page-request-related headers
             flags=["zyte-api"],
+            # API provides no page-request-related headers, so returning no headers
         )
 
     @inlineCallbacks
