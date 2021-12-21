6938244892d872e44c441914f290d81c6afeceae
sortafreel <woutut@gmail.com>
2021-12-21 17:39:58 +0200

    Add bumpversion.

--- setup.py
+++ setup.py
@@ -1,19 +1,12 @@
 import setuptools
 
-from scrapy_zyte_api import __version__
-
-
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
 setuptools.setup(
     name="scrapy-zyte-api",
-    version=__version__,
+    version="0.0.0",
     packages=["scrapy_zyte_api"],
-    install_requires=[
-        "scrapy>=2.0,!=2.4.0",
-        "zyte-api>=0.1.2",
-        "twisted>=21.7.0"
-    ],
+    install_requires=["scrapy>=2.0,!=2.4.0", "zyte-api>=0.1.2", "twisted>=21.7.0"],
 )
