27770881e739190655239e00644886851310f2f5
Adrián Chaves <adrian@chaves.io>
2023-01-25 13:25:00 +0100

    Bump version: 0.7.0 → 0.7.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="scrapy-zyte-api",
-    version="0.7.0",
+    version="0.7.1",
     description="Client library to process URLs through Zyte API",
     long_description=open("README.rst").read(),
     long_description_content_type="text/x-rst",
