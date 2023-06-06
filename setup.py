f1d56ebf2bfdd5263a980d0863ee6bedd08ef3ed
Andrey Rakhmatullin <wrar@wrar.name>
2023-06-06 21:01:47 +0400

    Bump the minimum packaging version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     url="https://github.com/scrapy-plugins/scrapy-zyte-api",
     packages=["scrapy_zyte_api"],
     install_requires=[
-        "packaging>=14.0",
+        "packaging>=14.1",
         "scrapy>=2.0.1",
         "scrapy-poet>=0.9.0",
         "web-poet>=0.13.0",
