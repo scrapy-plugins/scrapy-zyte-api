94d53f8e92239cd20eb4fb91a82174bbc9833ee3
Mikhail Korobov <kmike84@gmail.com>
2023-07-14 12:24:18 +0500

    [wip] Actions dependency

---
+++ scrapy_zyte_api/page_inputs.py
@@ -0,0 +1,49 @@
+from typing import Annotated, List, Optional, TypedDict
+
+import attrs
+
+
+class _Selector(TypedDict, total=False):
+    type: str
+    value: str
+    state: Optional[str]
+
+
+class _Action(TypedDict, total=False):
+    action: str
+    url: Optional[str]
+    urlPattern: Optional[str]
+    urlMatchingOptions: Optional[str]
+    selector: Optional[_Selector]
+    onError: Optional[str]
+    delay: Optional[float]
+    timeout: Optional[float]
+    id: Optional[str]
+    keyword: Optional[str]
+    key: Optional[str]
+    source: Optional[str]
+    options: Optional[dict]
+
+
+@attrs.define
+class _Actions:
+    result: Optional[List]
+
+
+@attrs.define
+class _ActionsList:
+    value: List[_Action]
+
+
+def Actions(actions: List[_Action]):
+    """
+    Add this dependency to your page object to execute Zyte API actions::
+
+        @attrs.define
+        class MyPage(ItemPage)
+            actions: Actions([
+                {"action": "scrollBottom"},
+            ])
+
+    """
+    return Annotated[_Actions, _ActionsList[actions]]
