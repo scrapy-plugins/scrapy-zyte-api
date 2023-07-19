febe99fb4c77822dc85968666da6256eeff5e1aa
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2023-07-19 19:13:30 +0800

    introduce ZYTE_API_MAX_REQUESTS

--- scrapy_zyte_api/_downloader_middleware.py
+++ scrapy_zyte_api/_downloader_middleware.py
@@ -1,4 +1,10 @@
+import logging
+
+from scrapy.exceptions import IgnoreRequest
+
 from ._params import _ParamParser
+
+logger = logging.getLogger(__name__)
 
 
 class ScrapyZyteAPIDownloaderMiddleware:
@@ -12,6 +18,13 @@ class ScrapyZyteAPIDownloaderMiddleware:
         self._param_parser = _ParamParser(crawler, cookies_enabled=False)
         self._crawler = crawler
 
+        self._max_requests = crawler.settings.getint("ZYTE_API_MAX_REQUESTS")
+        if self._max_requests:
+            logger.info(
+                f"Maximum Zyte API requests for this crawl is set at "
+                f"{self._max_requests}. Spider will close when it's reached."
+            )
+
     def process_request(self, request, spider):
         if self._param_parser.parse(request) is None:
             return
@@ -23,3 +36,24 @@ class ScrapyZyteAPIDownloaderMiddleware:
             request.meta["download_slot"] = slot_id
         _, slot = downloader._get_slot(request, spider)
         slot.delay = 0
+
+        if (
+            self._max_requests
+            and self._zapi_req_count() + self._download_req_count(downloader)
+            >= self._max_requests
+        ):
+            self._crawler.engine.close_spider(spider, "closespider_max_zapi_requests")
+            self._crawler.engine.downloader.close()
+            raise IgnoreRequest("Reached max Zyte API requests")
+
+    def _zapi_req_count(self) -> int:
+        return self._crawler.stats.get_value("scrapy-zyte-api/processed", 0)
+
+    def _download_req_count(self, downloader) -> int:
+        return sum(
+            [
+                len(slot.transferring)
+                for slot_id, slot in downloader.slots.items()
+                if slot_id.startswith(self._slot_prefix)
+            ]
+        )
