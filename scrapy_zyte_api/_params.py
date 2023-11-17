3567ee8a9b6aee7a197e46fa50b3a68276a7bf60
Adrián Chaves <adrian@chaves.io>
2023-11-17 12:28:10 +0100

    Define the browser and extract keys from a general mapping

--- scrapy_zyte_api/_params.py
+++ scrapy_zyte_api/_params.py
@@ -15,16 +15,75 @@ from ._cookies import _get_all_cookies
 
 logger = getLogger(__name__)
 
-_EXTRACT_KEYS = {
-    "article",
-    "articleList",
-    "articleNavigation",
-    "product",
-    "productList",
-    "productNavigation",
+_NoDefault = object()
+
+# Map of all known Zyte API request params and how they need to be handled.
+# Sorted by appearance in https://docs.zyte.com/zyte-api/usage/reference.html.
+_REQUEST_PARAMS = {
+    "url": {
+        "default": _NoDefault,
+        "is_extract_type": False,
+        "requires_browser_rendering": False,
+    },
+    "browserHtml": {
+        "default": False,
+        "is_extract_type": False,
+        "requires_browser_rendering": True,
+    },
+    "screenshot": {
+        "default": False,
+        "is_extract_type": False,
+        "requires_browser_rendering": True,
+    },
+    "article": {
+        "default": False,
+        "is_extract_type": True,
+        "requires_browser_rendering": False,
+    },
+    "articleList": {
+        "default": False,
+        "is_extract_type": True,
+        "requires_browser_rendering": False,
+    },
+    "articleNavigation": {
+        "default": False,
+        "is_extract_type": True,
+        "requires_browser_rendering": False,
+    },
+    "jobPosting": {
+        "default": False,
+        "is_extract_type": True,
+        "requires_browser_rendering": False,
+    },
+    "product": {
+        "default": False,
+        "is_extract_type": True,
+        "requires_browser_rendering": False,
+    },
+    "productList": {
+        "default": False,
+        "is_extract_type": True,
+        "requires_browser_rendering": False,
+    },
+    "productNavigation": {
+        "default": False,
+        "is_extract_type": True,
+        "requires_browser_rendering": False,
+    },
 }
-_BROWSER_KEYS = _EXTRACT_KEYS | {"browserHtml", "screenshot"}
-_DEFAULT_API_PARAMS = {key: False for key in _BROWSER_KEYS}
+
+_BROWSER_KEYS = {
+    key for key, value in _REQUEST_PARAMS.items() if value["requires_browser_rendering"]
+}
+_EXTRACT_KEYS = {
+    key for key, value in _REQUEST_PARAMS.items() if value["is_extract_type"]
+}
+_BROWSER_OR_EXTRACT_KEYS = _BROWSER_KEYS | _EXTRACT_KEYS
+_DEFAULT_API_PARAMS = {
+    key: value["default"]
+    for key, value in _REQUEST_PARAMS.items()
+    if value["default"] != _NoDefault
+}
 
 _DEFAULT_ACCEPT_ENCODING = ", ".join(
     encoding.decode() for encoding in ACCEPTED_ENCODINGS
@@ -149,7 +208,7 @@ def _set_request_headers_from_request(
         api_params.pop("customHttpRequestHeaders")
 
     if (
-        (not response_body or any(api_params.get(k) for k in _BROWSER_KEYS))
+        (not response_body or any(api_params.get(k) for k in _BROWSER_OR_EXTRACT_KEYS))
         and request_headers is not False
         or request_headers is True
     ):
@@ -167,7 +226,7 @@ def _set_http_response_body_from_request(
     api_params: Dict[str, Any],
     request: Request,
 ):
-    if not any(api_params.get(k) for k in _BROWSER_KEYS):
+    if not any(api_params.get(k) for k in _BROWSER_OR_EXTRACT_KEYS):
         api_params.setdefault("httpResponseBody", True)
     elif api_params.get("httpResponseBody") is False:
         logger.warning(
