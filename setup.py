368c529f5c93fef68cfd945c0de64b4be01a3947
Andrey Rakhmatullin <wrar@wrar.name>
2024-03-05 18:34:32 +0500

    Update to the latest scrapy-poet.

--- setup.py
+++ setup.py
@@ -31,8 +31,8 @@ setuptools.setup(
         # Sync with [testenv:pinned-provider] @ tox.ini
         "provider": [
             "andi>=0.6.0",
-            "scrapy-poet>=0.21.0",
-            "web-poet>=0.16.0",
+            "scrapy-poet>=0.22.0",
+            "web-poet>=0.17.0",
             "zyte-common-items>=0.8.0",
         ]
     },
