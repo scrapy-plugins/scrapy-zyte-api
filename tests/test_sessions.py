9fba274133fe1d8bccc2d444b5e8f9187267cace
Adrián Chaves <adrian@chaves.io>
2024-06-04 00:12:58 +0200

    Test bad init settings

--- tests/test_sessions.py
+++ tests/test_sessions.py
@@ -541,3 +541,88 @@ async def test_checker_location(postal_code, url, close_reason, stats, mockserve
     }
     assert crawler.spider.close_reason == close_reason
     assert session_stats == stats
+
+
+@pytest.mark.parametrize(
+    ("setting", "value"),
+    (
+        (0, 1),
+        (1, 1),
+        (2, 2),
+        (None, 8),
+    ),
+)
+@ensureDeferred
+async def test_max_bad_inits(setting, value, mockserver):
+    settings = {
+        "ZYTE_API_URL": mockserver.urljoin("/"),
+        "ZYTE_API_SESSION_ENABLED": True,
+        "ZYTE_API_SESSION_PARAMS": {"browserHtml": True, "httpResponseBody": True},
+    }
+    if setting is not None:
+        settings["ZYTE_API_SESSION_MAX_BAD_INITS"] = setting
+
+    class TestSpider(Spider):
+        name = "test"
+        start_urls = ["https://example.com"]
+
+        def parse(self, response):
+            pass
+
+    crawler = await get_crawler(settings, spider_cls=TestSpider, setup_engine=False)
+    await crawler.crawl()
+
+    session_stats = {
+        k: v
+        for k, v in crawler.stats.get_stats().items()
+        if k.startswith("scrapy-zyte-api/sessions")
+    }
+    assert session_stats == {
+        "scrapy-zyte-api/sessions/pools/example.com/init/failed": value,
+    }
+
+
+@pytest.mark.parametrize(
+    ("global_setting", "pool_setting", "value"),
+    (
+        (None, 0, 1),
+        (None, 1, 1),
+        (None, 2, 2),
+        (3, None, 3),
+    ),
+)
+@ensureDeferred
+async def test_max_bad_inits_per_pool(global_setting, pool_setting, value, mockserver):
+    settings = {
+        "ZYTE_API_URL": mockserver.urljoin("/"),
+        "ZYTE_API_SESSION_ENABLED": True,
+        "ZYTE_API_SESSION_PARAMS": {"browserHtml": True, "httpResponseBody": True},
+    }
+    if global_setting is not None:
+        settings["ZYTE_API_SESSION_MAX_BAD_INITS"] = global_setting
+    if pool_setting is not None:
+        settings["ZYTE_API_SESSION_MAX_BAD_INITS_PER_POOL"] = {
+            "pool.example": pool_setting
+        }
+
+    class TestSpider(Spider):
+        name = "test"
+        start_urls = ["https://example.com", "https://pool.example"]
+
+        def parse(self, response):
+            pass
+
+    crawler = await get_crawler(settings, spider_cls=TestSpider, setup_engine=False)
+    await crawler.crawl()
+
+    session_stats = {
+        k: v
+        for k, v in crawler.stats.get_stats().items()
+        if k.startswith("scrapy-zyte-api/sessions")
+    }
+    assert session_stats == {
+        "scrapy-zyte-api/sessions/pools/example.com/init/failed": (
+            8 if global_setting is None else global_setting
+        ),
+        "scrapy-zyte-api/sessions/pools/pool.example/init/failed": value,
+    }
