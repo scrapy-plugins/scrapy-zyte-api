6141a7113c83476b346885891fcbdc4354b440b8
Adrián Chaves <adrian@chaves.io>
2024-06-10 10:08:44 +0200

    Let provider params influece AnyResponse resolution (#200)

--- scrapy_zyte_api/providers.py
+++ scrapy_zyte_api/providers.py
@@ -174,8 +174,10 @@ class ZyteApiProvider(PageObjectInputProvider):
 
         if AnyResponse in to_provide:
             if (
-                item_requested and extract_from != "httpResponseBody"
-            ) or extract_from == "browserHtml":
+                (item_requested and extract_from != "httpResponseBody")
+                or extract_from == "browserHtml"
+                or zyte_api_meta.get("browserHtml", False) is True
+            ):
                 html_requested = True
             elif extract_from == "httpResponseBody" or http_response_needed:
                 zyte_api_meta["httpResponseBody"] = True
