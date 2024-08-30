a89f3b72d3e6e28565292c11afed6c8d74c891ff
Adrián Chaves <adrian@chaves.io>
2024-08-30 10:15:31 +0200

    Improve code readability

--- scrapy_zyte_api/_poet_item_pipelines.py
+++ scrapy_zyte_api/_poet_item_pipelines.py
@@ -29,7 +29,7 @@ class ScrapyZyteAPIAutoFieldStatsItemPipeline:
         self._seen: Set[Type] = set()
         self._crawler = crawler
         self._stats = crawler.stats
-        self._cls_without_url: Set[Type] = set()
+        self._item_cls_without_url: Set[Type] = set()
 
     def open_spider(self, spider):
         for component in self._crawler.engine.downloader.middleware.middlewares:
@@ -42,17 +42,17 @@ class ScrapyZyteAPIAutoFieldStatsItemPipeline:
         )
 
     def process_item(self, item: Any, spider: Spider):
-        cls = item.__class__
+        item_cls = item.__class__
 
-        url_field = self._url_fields.get(cls, "url")
+        url_field = self._url_fields.get(item_cls, "url")
         adapter = ItemAdapter(item)
         url = adapter.get(url_field, None)
         if not url:
-            if cls not in self._cls_without_url:
-                self._cls_without_url.add(cls)
+            if item_cls not in self._item_cls_without_url:
+                self._item_cls_without_url.add(item_cls)
                 logger.warning(
-                    f"An item of type {cls} was missing a non-empty URL in "
-                    f"its {url_field!r} field. An item URL is necessary to "
+                    f"An item of type {item_cls} was missing a non-empty URL "
+                    f"in its {url_field!r} field. An item URL is necessary to "
                     f"determine the page object that was used to generate "
                     f"that item, and hence print the auto field stats that "
                     f"you requested by enabling the ZYTE_API_AUTO_FIELD_STATS "
@@ -62,9 +62,9 @@ class ScrapyZyteAPIAutoFieldStatsItemPipeline:
                 )
             return
 
-        page_cls = self._registry.page_cls_for_item(url, cls)
-        cls = page_cls or cls
+        page_cls = self._registry.page_cls_for_item(url, item_cls)
 
+        cls = page_cls or item_cls
         if cls in self._seen:
             return
         self._seen.add(cls)
@@ -74,8 +74,8 @@ class ScrapyZyteAPIAutoFieldStatsItemPipeline:
         else:
             auto_fields = set()
             missing_fields = False
-            for field_name in get_fields_dict(cls):
-                if is_auto_field(cls, field_name):  # type: ignore[arg-type]
+            for field_name in get_fields_dict(page_cls):
+                if is_auto_field(page_cls, field_name):  # type: ignore[arg-type]
                     auto_fields.add(field_name)
                 else:
                     missing_fields = True
