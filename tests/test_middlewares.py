e0734395ab62109d8cb07dbc89be0b742fcb9e38
Andrey Rakhmatullin <wrar@wrar.name>
2024-11-04 14:58:57 +0500

    Fix a new typing issue.

--- tests/test_middlewares.py
+++ tests/test_middlewares.py
@@ -216,6 +216,7 @@ async def test_max_requests_race_condition(caplog):
         f"Maximum Zyte API requests for this crawl is set at {zapi_max_requests}"
         in caplog.text
     )
+    assert crawler.stats
     assert crawler.stats.get_value("scrapy-zyte-api/success") == zapi_max_requests
     assert crawler.stats.get_value("scrapy-zyte-api/processed") == zapi_max_requests
     assert crawler.stats.get_value("item_scraped_count") == zapi_max_requests
