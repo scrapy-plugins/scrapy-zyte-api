5c94151bd7a2a6cb7220bb0112f67982b7669523
Adrian Chaves <adrian@zyte.com>
2026-01-12 08:53:35 +0100

    Remove unneeded @deferred_f_from_coro_f

--- tests/mockserver.py
+++ tests/mockserver.py
@@ -11,7 +11,6 @@ from subprocess import PIPE, Popen
 from typing import Dict, List, Optional
 from urllib.parse import urlparse
 
-from scrapy.utils.defer import deferred_f_from_coro_f
 from scrapy import Request
 from twisted.internet import reactor
 from twisted.internet.defer import Deferred
@@ -41,7 +40,6 @@ def get_ephemeral_port():
     return s.getsockname()[1]
 
 
-@deferred_f_from_coro_f
 async def produce_request_response(mockserver, meta, settings=None):
     settings = settings if settings is not None else {**SETTINGS}
     async with mockserver.make_handler(settings) as handler:
